3d19980dcfbbaf17d1c6a02e793e02a777115380
MotiMashiah <moti2424@gmail.com>
2024-07-16 13:19:25 -0400

    added test controller

--- Controllers/DataController.cs
+++ Controllers/DataController.cs
@@ -12,6 +12,8 @@ using Microsoft.Extensions.Logging;
 using MSRecordsEngine.Controllers;
 using MSRecordsEngine.Services;
 using Leadtools.ImageProcessing.Core;
+using Smead.Security;
+using System.Data.Entity.Validation;
 namespace FusionWebApi.Controllers
 {
     [Route("[controller]")]
@@ -28,19 +30,30 @@ namespace FusionWebApi.Controllers
             _commonService = commonControllersService;
         }
 
-        [HttpGet]
-        public void test()
+        [HttpPost]
+        [Route("test")]
+        public void test(MicroArguments model)
         {
+            var pass = model.passport;
             try
             {
-                throw new Exception("hello I am failure in ms record manager");
+                throw new Exception("hello this is an error message!");
             }
             catch (Exception ex)
             {
                 _commonService.Logger.LogError(ex.Message);
-                throw new Exception(ex.Message, ex);
+                throw;
             }
         }
 
+    }
+
+    public class MicroArguments
+    {
+        public object model { get; set; }
+        public Passport passport { get; set; }
+        public List<string> liststring { get; set; }
+        public List<int> listints { get; set; }
+        public List<object> listobject { get; set; }
     }
 }
